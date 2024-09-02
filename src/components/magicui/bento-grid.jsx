e9e55494b4e09e66420eef4f22434060f61991e7
Rajvardhan <srajvardhanv@gmail.com>
2024-09-03 01:01:14 +0530

    Added several external libraries

--- src/components/magicui/bento-grid.jsx
+++ src/components/magicui/bento-grid.jsx
@@ -23,7 +23,7 @@ const BentoCard = ({
   name,
   className,
   background,
-  Icon,
+  Icon, // This can be null or undefined
   description,
   href,
   cta,
@@ -49,10 +49,12 @@ const BentoCard = ({
     >
       <div>{background}</div>
       <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-        <Icon
-          className="h-12 w-12 origin-left transform-gpu transition-all duration-300 ease-in-out group-hover:scale-75"
-          style={{ color: textColor }} // Set icon color
-        />
+        {Icon && (
+          <Icon
+            className="h-12 w-12 origin-left transform-gpu transition-all duration-300 ease-in-out group-hover:scale-75"
+            style={{ color: textColor }} // Set icon color
+          />
+        )}
         <h3 className="text-xl font-semibold" style={{ color: textColor }}>
           {name}
         </h3>
@@ -61,24 +63,12 @@ const BentoCard = ({
         </p>
       </div>
 
-      {/* Uncomment if needed */}
-      {/* <div
-        className={cn(
-          "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100"
-        )}
-      >
-        <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
-          <a href={href}>
-            {cta}
-            <ArrowRightIcon className="ml-2 h-4 w-4" style={{ color: textColor }} />
-          </a>
-        </Button>
-      </div> */}
       <div
         className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10"
       />
     </div>
   );
 };
+
 
 export { BentoCard, BentoGrid };
--- src/components/magicui/marquee.jsx
+++ src/components/magicui/marquee.jsx
@@ -13,7 +13,7 @@ export default function Marquee({
     (<div
       {...props}
       className={cn(
-        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+        "group flex overflow-hidden p-2 [--duration:100s] [--gap:1rem] [gap:var(--gap)]",
         {
           "flex-row": !vertical,
           "flex-col": vertical,
