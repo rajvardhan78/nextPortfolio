67979e329e57807cad62ff84b45f9f4053c8e9fb
Rajvardhan <srajvardhanv@gmail.com>
2024-09-03 00:58:24 +0530

    Updated tailwind.config.js as per external assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,14 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -113,6 +121,8 @@ module.exports = {
         third: "moveInCircle 40s linear infinite", // Added line
         fourth: "moveHorizontal 40s ease infinite", // Added line
         fifth: "moveInCircle 20s ease infinite", // Added line
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
       },
     },
   },
